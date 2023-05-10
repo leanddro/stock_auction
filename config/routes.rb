6af2c79ad166b61b5dbc2fe9d13586be071c18ca
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-10 14:18:13 -0300

    Rotas de categorias

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
+  resources :categories, only: %i(index new create update show) do
+    post 'toggle', on: :member
+  end
 end
