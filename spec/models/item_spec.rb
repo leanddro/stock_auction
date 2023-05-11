8ede08c7105bdbd3436b84271b6ac9798f5763e4
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-11 14:32:07 -0300

    Validando status padrão

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,6 +23,14 @@ RSpec.describe Item, type: :model do
       expect(result).to be true
     end
 
+    it 'o status deve ser :available' do
+      user = User.create!(name: 'Admir', cpf: '054.732.630-03', email: 'admir@leilaodogalpao.com.br', password: 'password')
+      category = Category.create!(name: 'Eletrônico', description: 'Incididunt officia occaecat consequat aliquip pariatur aute culpa.')
+      item = Item.create!(name: 'Notebook Gamer Acer Nitro 5', description: 'Intel Core i7-11800H, GeForce GTX 1650, 8GB RAM, SSD 512GB, 15.6 Full HD 144Hz IPS, Windows 11, Preto - AN515-57-740K', weight: 2300, width: 363.4, height: 24.35, depth: 255, category: category, create_by: user)
+
+      expect(item.status).to eq 'available'
+    end
+
   end
 
   describe 'Deve gera código aleatório' do
