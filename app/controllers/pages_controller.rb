e29e90f7dbcdeb92e5971465968f7138b4b63633
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-15 23:54:28 -0300

    Adicionando listagem de lotes e detalhes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @current_batches = Batch.where(start_at: ..DateTime.now, status: ['approved'])
+    @next_batches = Batch.where(start_at: DateTime.now.next_day(1).., status: ['approved', 'pending'])
+  end
+
+  def batch_detail
+    @batch = Batch.find(params[:id])
   end
 end
