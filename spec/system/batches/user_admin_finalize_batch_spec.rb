dc0eb51a8eb0198b1f09aab2d7b5791f78351c32
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-17 17:28:10 -0300

    Ajuste

--- spec/system/batches/user_admin_finalize_batch_spec.rb
+++ spec/system/batches/user_admin_finalize_batch_spec.rb
@@ -7,10 +7,12 @@ describe 'Usuário admin finaliza lote' do
     category = Category.create!(name: 'Eletrônico', description: 'Incididunt officia occaecat consequat aliquip pariatur aute culpa.')
 
     item = Item.create!(name: 'Notebook Gamer Acer Nitro 5', description: 'Intel Core i7-11800H, GeForce GTX 1650, 8GB RAM, SSD 512GB, 15.6 Full HD 144Hz IPS, Windows 11, Preto - AN515-57-740K', weight: 2300, width: 36.34, height: 2.435, depth: 25.5, category: category, create_by: admin)
+    batch = nil
+    travel_to DateTime.now.prev_month do
+      batch = Batch.new(code: 'AcSx*1-mL', start_at: DateTime.now.next_day, end_at: DateTime.now.next_day(10), minimum_bid: 100, minimum_bid_difference: 10, create_by: admin)
+      batch.approved!
+    end
 
-    batch = Batch.new(code: 'AcSx*1-mL', start_at: DateTime.now.prev_day(50), end_at: DateTime.now.prev_day(10), minimum_bid: 100, minimum_bid_difference: 10, create_by: admin)
-    batch.save(validate: false)
-    batch.approved!
     item.batch = batch
     item.unavailable!
 
@@ -37,8 +39,8 @@ describe 'Usuário admin finaliza lote' do
 
     item = Item.create!(name: 'Notebook Gamer Acer Nitro 5', description: 'Intel Core i7-11800H, GeForce GTX 1650, 8GB RAM, SSD 512GB, 15.6 Full HD 144Hz IPS, Windows 11, Preto - AN515-57-740K', weight: 2300, width: 36.34, height: 2.435, depth: 25.5, category: category, create_by: admin)
 
-    batch = Batch.new(code: 'AcSx*1-mL', start_at: DateTime.now, end_at: DateTime.now.next_day(20), minimum_bid: 100, minimum_bid_difference: 10, create_by: admin)
-    batch.save(validate: false)
+    batch = Batch.new(code: 'AcSx*1-mL', start_at: DateTime.now.next_day, end_at: DateTime.now.next_day(20), minimum_bid: 100, minimum_bid_difference: 10, create_by: admin)
+
     batch.approved!
     item.batch = batch
     item.unavailable!
