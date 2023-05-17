5810808f1d7d95f5db86a5e67cd1ea208649a9e6
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-17 15:34:33 -0300

    Metodos de finish e cancel

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class BatchesController < AdminController
+  before_action :set_batch, only: %i(approve finish add cancel)
+
   def index
     @batches = Batch.all
   end
@@ -20,30 +24,52 @@ class BatchesController < AdminController
   def show
     @batch = Batch.find(params[:id])
     @items = Item.available
+    @last_bid = @batch.bids.last
   end
 
   def approve
-    batch = Batch.find(params[:id])
-    if batch.create_by != current_user && batch.pending? && batch.end_at > DateTime.now
-      batch.approved_by = current_user
-      batch.approved!
-      return redirect_to batch, notice: t('.success')
+    if @batch.create_by != current_user && @batch.pending? && @batch.end_at > DateTime.now
+      @batch.approved_by = current_user
+      @batch.approved!
+      return redirect_to @batch, notice: t('.success')
     end
-    redirect_to batch, alert: t('.failed')
+    redirect_to @batch, alert: t('.failed')
+  end
+
+  def finish
+    return redirect_to @batch, alert: t('.erro_status') unless @batch.approved?
+    return redirect_to @batch, alert: t('.erro_date') if @batch.end_at > DateTime.now
+    return redirect_to @batch, notice: t('.not_bids') unless @batch.bids.present?
+
+    @batch.finished!
+    @batch.items.each(&:sealed!)
+    @batch.winner_id = @batch.bids.last.user_id
+    @batch.save!
+    redirect_to @batch, notice: t('.success')
+  end
+
+  def cancel
+    p @batch.bids
+    return redirect_to @batch, alert: t('.have_bids') if @batch.bids.present?
+    @batch.canceled!
+    redirect_to @batch, notice: t('.success')
   end
 
   def add
-    batch = Batch.find(params[:id])
     item = Item.find(params[:item_id])
     if item_available?(item)
-      item.batch = batch
+      item.batch = @batch
       item.unavailable!
-      return redirect_to batch, notice: t('.success')
+      return redirect_to @batch, notice: t('.success')
     end
-    redirect_to batch, alert: t('.failed')
+    redirect_to @batch, alert: t('.failed')
   end
 
   private
+
+  def set_batch
+    @batch = Batch.find(params[:id])
+  end
 
   def item_available?(item)
     return true if item.available? && item.batch_id.nil?
