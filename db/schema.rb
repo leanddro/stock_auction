2bf9e5e69ca5372cfabfa8cdcbf95213798f1ff5
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-18 13:26:14 -0300

    Alterações pra funcionamento dos comando do Readme

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_172214) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,18 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_172214) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "batch_items", force: :cascade do |t|
-    t.integer "batch_id", null: false
-    t.integer "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["batch_id"], name: "index_batch_items_on_batch_id"
-    t.index ["item_id"], name: "index_batch_items_on_item_id"
   end
 
   create_table "batches", force: :cascade do |t|
@@ -123,8 +114,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_172214) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "batch_items", "batches"
-  add_foreign_key "batch_items", "items"
   add_foreign_key "batches", "users", column: "approved_by_id"
   add_foreign_key "batches", "users", column: "create_by_id"
   add_foreign_key "batches", "users", column: "winner_id"
